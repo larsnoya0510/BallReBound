516274c96d17278e0b28983078600169503cc37c
larsnoya <larsnoya@gmail.com>
2020-04-06 14:36:46 +0800

    feat : add auto clear feature

--- app/src/main/java/com/example/ballrebound/MainActivity.kt
+++ app/src/main/java/com/example/ballrebound/MainActivity.kt
@@ -27,6 +27,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ballList = mutableListOf<Ball>()
         showBallCount()
+        dispearbleSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                ballList.forEach { it.isScideble=true }
+            }
+            else{
+                ballList.forEach { it.isScideble=false }
+            }
+        }
         activity_main_btn_add.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 var mBall = Ball(this@MainActivity)
@@ -36,22 +44,22 @@ class MainActivity : AppCompatActivity() {
         })
         activity_main_btn_end.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                for(i in 0 until ballList.size) {
+                for (i in 0 until ballList.size) {
                     ballList[i].stop()
                 }
             }
         })
         activity_main_btn_start.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                for(i in 0 until ballList.size) {
+                for (i in 0 until ballList.size) {
                     ballList[i].start()
                 }
             }
         })
         activity_main_btn_clear.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                for(i in 0 until ballList.size) {
-                   var  relative = findViewById<RelativeLayout>(R.id.activity_main_relativelayout)
+                for (i in 0 until ballList.size) {
+                    var relative = findViewById<RelativeLayout>(R.id.activity_main_relativelayout)
                     relative!!.removeView(ballList[i].imageView)
                     ballList[i].mThread.quitSafely()
                 }
@@ -64,37 +72,61 @@ class MainActivity : AppCompatActivity() {
     private fun showBallCount() {
         ballCountTextView.text = "Ball : ${ballList.size}"
     }
-
+    fun killBalls(mBall:Ball){
+        var index=ballList.indexOfFirst { it==mBall}
+        mBall.mThread.quitSafely()
+        var relative = findViewById<RelativeLayout>(R.id.activity_main_relativelayout)
+        relative!!.removeView(ballList[index].imageView)
+        ballList.remove(mBall)
+        showBallCount()
+    }
     companion object {
         @kotlin.jvm.JvmField
         var MOVE_IMAGE: Int = 1
     }
 
-    class Ball(var context:Context) {
+    class Ball(var context: Context) {
+        var sucideCount=20
         var handler: Handler? = null
         var MOVE_IMAGE = 1
         //初始位置
-        var orginX=Random.nextInt(0,200)
-        var orginY=Random.nextInt(0,200)
+        var orginX = Random.nextInt(0, 200)
+        var orginY = Random.nextInt(0, 200)
         // 移動方向和距離
-        var decX = Random.nextInt(1,5)
-        var decY = Random.nextInt(1,5)
+        var decX = Random.nextInt(1, 5)
+        var decY = Random.nextInt(1, 5)
         // 座標
         var moveX: Int = 0
         var moveY: Int = 0
         var isMove: Boolean = false// 是否正在移動
+        var isScideble: Boolean = false
         var relative: RelativeLayout? = null
         var imageView: ImageView? = null
-        var mThread:HandlerThread = HandlerThread("BallThread")
+        var mThread: HandlerThread = HandlerThread("BallThread")
         var ballHandler = Handler()
         var ballRunnable: Runnable = Runnable {
             while (this.isMove) {
                 moveX += decX;
                 moveY += decY;
-                if ((moveX + imageView!!.getWidth()) >= relative!!.getWidth()  ) { decX = -decX; }
-                if(moveX < 0){ decX = -decX; }
-                if ((moveY + imageView!!.getHeight() ) >= relative!!.getHeight()) { decY = -decY }
-                if (moveY < 0) { decY = -decY }
+                if ((moveX + imageView!!.getWidth()) >= relative!!.getWidth()) {
+                    decX = -decX;
+                   if(this.isScideble) sucideCount--
+                }
+                if (moveX < 0) {
+                    decX = -decX;
+                    if(this.isScideble) sucideCount--
+                }
+                if ((moveY + imageView!!.getHeight()) >= relative!!.getHeight()) {
+                    decY = -decY
+                    if(this.isScideble) sucideCount--
+                }
+                if (moveY < 0) {
+                    decY = -decY
+                    if(this.isScideble) sucideCount--
+                }
+                if(sucideCount<=0){
+                    sucide()
+                }
                 var message = Message();
                 message.what = MOVE_IMAGE;
 
@@ -111,48 +143,58 @@ class MainActivity : AppCompatActivity() {
             }
         }
         var boundRate: Float = 0F
+
         init {
-            moveX=orginX
-            moveY=orginY
+            moveX = orginX
+            moveY = orginY
             imageView = ImageView(context!!)
             imageView!!.setImageDrawable(context.resources.getDrawable(R.drawable.ic_soccer_ball))
             var mLayoutParams = RelativeLayout.LayoutParams(
-            50,50
+                50, 50
             )
-            mLayoutParams.setMargins(orginX, 10, 0,orginY)
+            mLayoutParams.setMargins(orginX, 10, 0, orginY)
             imageView!!.layoutParams = mLayoutParams
             relative = (context as MainActivity).findViewById<RelativeLayout>(R.id.activity_main_relativelayout)
             relative!!.addView(imageView)
             setColor(imageView!!)
-            handler = MyHandler(context as MainActivity,imageView)
+            handler = MyHandler(context as MainActivity, imageView)
             mThread.start()
             ballHandler = Handler(mThread.getLooper())
         }
-        fun setColor(mImageView:ImageView){
-            var r= Random.nextInt(0,255)
-            var g= Random.nextInt(0,255)
-            var b= Random.nextInt(0,255)
-            var mColor = Color.rgb(r,g,b)
+
+        fun setColor(mImageView: ImageView) {
+            var r = Random.nextInt(0, 255)
+            var g = Random.nextInt(0, 255)
+            var b = Random.nextInt(0, 255)
+            var mColor = Color.rgb(r, g, b)
             val bottomLayer = ColorDrawable(mColor) as Drawable
-            val topLayer = context.resources.getDrawable(R.drawable.ic_soccer_ball,null)
+            val topLayer = context.resources.getDrawable(R.drawable.ic_soccer_ball, null)
             val playWithBackground = LayerDrawable(arrayOf(bottomLayer, topLayer))
             Glide.with(context)
                 .load(playWithBackground)
                 .apply(RequestOptions.circleCropTransform())
                 .into(mImageView)
         }
+
         fun start() {
             this.isMove = false;
-             if (!this.isMove) {
-                 this.isMove = true;
-             } else {
-                 return;
-             }
-            if(!ballHandler.hasCallbacks(ballRunnable))   ballHandler.post(ballRunnable)
+            if (!this.isMove) {
+                this.isMove = true;
+            } else {
+                return;
+            }
+            if (!ballHandler.hasCallbacks(ballRunnable)) ballHandler.post(ballRunnable)
         }
-        fun stop(){
-            this.isMove=false
+
+        fun stop() {
+            this.isMove = false
             ballHandler.removeCallbacks(ballRunnable)
+        }
+        fun sucide(){
+            this.stop()
+            (this.context as MainActivity).runOnUiThread{
+                (this.context as MainActivity).killBalls(this)
+            }
         }
     }
 }
